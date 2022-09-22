2bda4a705bce2f1c2963220441b56bc8113b3a37
alexihavr <dchavronin@bk.ru>
2022-09-22 15:00:30 +0300

    rename typeNumber to tubeNumber

--- client/src/redux/vtds/reducer.ts
+++ client/src/redux/vtds/reducer.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { getDefaultColumn } from '../../helpers/excel';
 import { getAddedColumnTable } from '../../helpers/reducers';
 
 import { getPipelineTable, getVtds } from './thunks';
